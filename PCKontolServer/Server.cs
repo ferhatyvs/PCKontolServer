af2b6fdd694604cefd5b32335c62ce2bd6e63631
Ferhat Yavaş <ferhatyavas46@outlook.com>
2018-12-05 14:10:46 +0300

    Guncelleme

--- PCKontolServer/Server.cs
+++ PCKontolServer/Server.cs
@@ -5,9 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace PCKontolServer
 {
+    public class Test
+    {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+        private const int APPCOMMAND_MEDIA_PLAY_PAUSE = 0xE00000;
+        IntPtr Handle = Process.GetCurrentProcess().MainWindowHandle;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
+        public void Mute()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        public void VolDown()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        public void VolUp()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+        public void PPause()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_MEDIA_PLAY_PAUSE);
+        }
+    }
     class Server
     {
         private static TcpListener listener { get; set; }
@@ -15,29 +53,31 @@ namespace PCKontolServer
 
         public static void StartServer(int port)
         {
-            IPAddress address = IPAddress.Parse("127.0.0.1");
+            Console.WriteLine("Ip Adresini Giriniz:");
+            IPAddress address = IPAddress.Parse(Console.ReadLine());
             listener = new TcpListener(address, port);
 
             listener.Start();
             accept = true;
 
-            Console.WriteLine($"Server started. Listening to TCP clients at 127.0.0.1:{port}");
+            Console.WriteLine($"Server Baslatildi, Port=" + port);
         }
 
         public static void Listen()
         {
+            Test tus = new Test();
             if (listener != null && accept)
             {
 
                 // Continue listening.  
                 while (true)
                 {
-                    Console.WriteLine("Waiting for client...");
+                    Console.WriteLine("Baglanti Bekleniyor..");
                     var clientTask = listener.AcceptTcpClientAsync(); // Get the client  
 
                     if (clientTask.Result != null)
                     {
-                        Console.WriteLine("Client connected. Waiting for data.");
+                        Console.WriteLine("Baglanti tamamlandi, Veriler bekleniyor.");
                         var client = clientTask.Result;
                         string message = "";
 
@@ -51,6 +91,22 @@ namespace PCKontolServer
 
                             message = Encoding.ASCII.GetString(buffer);
                             Console.WriteLine(message);
+                            if (message.StartsWith("volumeup"))
+                            {
+                                tus.VolUp();
+                            }
+                            else if (message.StartsWith("volumedown"))
+                            {
+                                tus.VolDown();
+                            }
+                            else if (message.StartsWith("mute"))
+                            {
+                                tus.Mute();
+                            }
+                            else if (message.StartsWith("ppause"))
+                            {
+                                tus.PPause();
+                            }
                         }
                         Console.WriteLine("Closing connection.");
                         client.GetStream().Dispose();
